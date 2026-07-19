3ceb6098b32ccec5665b6abfad72369341154a10
Янин Ярослав <yaroslav.i.yanin@gmail.com>
2026-07-19 20:12:47 +0500

    добавление хеш-функции для строк

--- lib/src/lib_main.c
+++ lib/src/lib_main.c
@@ -4,5 +4,16 @@ lib_main.c - Реализация библиотеки хеш-таблицы и 
 Группа МК-101
 */
 #include "lib_main.h"
-#include <stdio.h>
-#include <stdlib.h>
+
+// Функция вычисляет хеш-сумму для строки по алгоритму djb2
+// key - указатель на нуль-терминированную строку
+size_t hash_djb2(const char* key) {
+    size_t hash = 5381;
+    int c;
+
+    while ((c = *key++)) {
+        hash = ((hash << 5) + hash) + c; // эквивалентно hash * 33 + c
+    }
+
+    return hash;
+}
